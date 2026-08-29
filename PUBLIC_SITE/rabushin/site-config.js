0ace4e35e4ca540b49e5d69523fc1dea8b3745c8
redmarine84 <52855152+redmarine84@users.noreply.github.com>
2026-08-29 13:23:22 -0400

    Update website URL in site-config.js

--- PUBLIC_SITE/rabushin/site-config.js
+++ PUBLIC_SITE/rabushin/site-config.js
@@ -1,7 +1,7 @@
 window.RABUSHIN_SITE_CONFIG = {
   appName: "RabuShin AI Game Master",
   operatorName: "RabuShinAIGM",
-  websiteUrl: "https://redmarine84.github.io/AppDownload.github.io/rabushin/",
+  websiteUrl: "https://redmarine84.github.io/Quests-of-Rabu-Shin/",  
     supportServerUrl: "https://discord.gg/kRj5K8GBu",
   supportEmail: "",
   effectiveDate: "August 29, 2026"
