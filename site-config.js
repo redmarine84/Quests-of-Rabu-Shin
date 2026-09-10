f6005012b3d4452f634995c72d7ea76bb1e309d9
redmarine84 <52855152+redmarine84@users.noreply.github.com>
2026-09-10 09:50:30 -0400

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -2,7 +2,7 @@ window.RABUSHIN_SITE_CONFIG = {
   appName: "RabuShin AI Game Master",
   operatorName: "RabuShinAIGM",
   websiteUrl: "https://redmarine84.github.io/Quests-of-Rabu-Shin/",  
-    supportServerUrl: "https://discord.gg/fA6Yt7Pnj" // Was: "https://discord.gg/kRj5K8GBu",
+    supportServerUrl: "https://discord.gg/fA6Yt7Pnj", // Was: "https://discord.gg/kRj5K8GBu",
   supportEmail: "",
   effectiveDate: "August 29, 2026"
 };
